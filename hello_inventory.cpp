a4ac049cae10e0cdccc749d2312866946e5553f1
wigdanmoharram <wigdan_moharram@berkeley.edu>
2025-11-23 15:30:48 -0800

    Add files via upload

--- hello_inventory.cpp
+++ hello_inventory.cpp
@@ -1,12 +1,66 @@
-#include <iostream>
-using namespace std;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-int main() {
-    // cout prints to the screen
-    cout << "Hello Inventory System!\n";
-    cout << "\tData Types:\n";
-    cout << "Size of int: " << sizeof(int) << " bytes\n";
-    cout << "Size of float: " << sizeof(float) << " bytes\n";
-    cout << "Size of char: " << sizeof(char) << " bytes\n";
-    return 0;
-}
+    <meta property="og:url" content="https://www.jdoodle.com" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="JDoodle - Online Compiler, Editor for Java, C/C++, etc" />
+    <meta
+      property="og:description"
+      content="JDoodle is an Online Compiler, Editor, IDE  for Java, C, C++, PHP, Perl, Python, Ruby and many more.  You can run your programs on the fly online, and you can save and share them with others. Quick and Easy way to compile and run programs online."
+    />
+    <meta property="og:image" content="https://www.jdoodle.com/assets/images/JD_banner.png" />
+    <meta property="og:image:alt" content="JDoodle" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="JDoodle - Online Compiler, Editor for Java, C/C++, etc" />
+    <meta
+      name="twitter:description"
+      content="JDoodle is an Online Compiler, Editor, IDE  for Java, C, C++, PHP, Perl, Python, Ruby and many more.  You can run your programs on the fly online, and you can save and share them with others. Quick and Easy way to compile and run programs online."
+    />
+    <meta name="twitter:image" content="https://www.jdoodle.com/assets/images/JD_banner.png" />
+    <meta name="google-site-verification" content="UvObv0WXViGO2pEP8egoe3k1sXqHcsSeAfEEjnUyuXg" />
+    <meta name="msvalidate.01" content="93539EB8D681BD90594A81FCAF6560BC" />
+
+    <link rel="icon" href="/favicon.ico" />
+
+    <title>
+      Online Compiler and Editor/IDE for Java, C, C++, PHP, Python, Ruby, Perl - Code and Run Online
+    </title>
+
+    <script type="application/ld+json">
+      {
+        "@context": "http://schema.org",
+        "@type": "SoftwareApplication",
+        "name": "JDoodle - Online Compiler And Editor",
+        "image": "https://www.jdoodle.com/assets/jdoodle-og.jpeg"
+      }
+    </script>
+    <script type="module" crossorigin src="/assets/index-905bdd37.js"></script>
+    <link rel="stylesheet" href="/assets/index-3c5107cf.css">
+  </head>
+
+  <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-W3NML658"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe
+    ></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <noscript>
+      <strong
+        >We're sorry but jdoodle-spa doesn't work properly without JavaScript enabled. Please enable
+        it to continue.</strong
+      >
+    </noscript>
+    <div id="app"></div>
+    
+  <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"9a345cdba9e96c97","serverTiming":{"name":{"cfExtPri":true,"cfEdge":true,"cfOrigin":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}},"version":"2025.9.1","token":"642255d447fd46dbbcd67f3ff9dbdf59"}' crossorigin="anonymous"></script>
+</body>
+</html>
