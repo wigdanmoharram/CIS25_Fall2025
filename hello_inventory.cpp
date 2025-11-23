a39d7e9d885a28d61d91587b1f8e6401c3f432f7
wigdanmoharram <wigdan_moharram@berkeley.edu>
2025-11-23 13:13:54 -0800

    Update hello_inventory.cpp

--- hello_inventory.cpp
+++ hello_inventory.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    // cout prints to the screen
     cout << "Hello Inventory System!\n";
     cout << "\tData Types:\n";
     cout << "Size of int: " << sizeof(int) << " bytes\n";
